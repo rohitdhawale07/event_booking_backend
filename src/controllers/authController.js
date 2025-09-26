9505ee54e93a159f7891b0a742f6d88761eda50c
rohitdhawale07 <rohitdhawale07@gmail.com>
2025-09-26 21:18:18 +0530

    e params added for register api

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,9 +4,9 @@ const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
   try {
-    const { mobile, email, password } = req.body;
+    const { mobile, email, password, name } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ mobile, email, password: hashedPassword });
+    const user = new User({ mobile, email, name, password: hashedPassword });
     await user.save();
     res.status(201).json({ message: 'User registered successfully' });
   } catch (err) {
